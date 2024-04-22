ece06f1cb60ea513f2e96406d011ccab707dfe9c
aishatariq1980 <167800892+aishatariq1980@users.noreply.github.com>
2024-04-22 22:39:59 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+let famouspersone = "Albert Einstein ";
+let message = "A person who never made a mistake never tired anything new.";
+console.log = (famouspersone, message);
---
+++ main.ts
@@ -0,0 +1,3 @@
+let famouspersone = "Albert Einstein " ;
+let message = "A person who never made a mistake never tired anything new." ;
+console.log = (famouspersone , message) ;
